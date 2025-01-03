70acf96fbc9ca2aa447b26c5f73a9c9fed7ddeee
Akhilesh <akhilesh.rathnakaran@rrd.com>
2025-01-03 12:06:39 +0530

    day17

---
+++ day17/HelloWorld.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class Hello
+{
+    static void Main(string[] args)
+    {
+        int Number = Convert.ToInt32(Console.ReadLine());
+        for (int i = 0; i < Number; i++)
+        {
+            Console.WriteLine("Hello World");
+
+        }
+    }
+}
