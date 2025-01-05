7687c1f420bdb3ed9c036d11d2ba9957278fccf6
Akhilesh <akhilesh.rathnakaran@rrd.com>
2025-01-05 21:28:38 +0530

    day18

---
+++ day17/HelloApp/HelloApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class hello
+{ 
+static void Main(string[] userNames)
+{
+    //if (userNames.Length == 0)
+    //{
+    //    Console.WriteLine("No arguments passed!");
+    //    //return;
+
+    //}
+    
+    {
+        foreach (string userName in userNames)
+            Console.WriteLine("Hello" + userName + "!");
+    }
+}
---
+++ day18/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class User
+{
+      static void Main(string[] userNames)
+    {
+        if (userNames.Length == 0)
+        {
+            Console.WriteLine("No arguments passed!");
+            return;
+
+        }
+        foreach (string userName in userNames)
+            Console.WriteLine("Hello " + userName + "!");
+        Console.WriteLine("");
+        {
+            for (int index = userNames.Length-1; index >= 0; index--)
+                //userNames[index] = userNames[index].ToUpper();
+            Console.WriteLine(userNames[index].ToUpper());
+
+        }
+    }
+
+}
