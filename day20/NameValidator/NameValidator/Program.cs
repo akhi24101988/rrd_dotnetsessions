794cc38c5ab2490af1d51ce7ae212aa056ded9f5
Akhilesh <akhilesh.rathnakaran@rrd.com>
2025-01-09 09:35:57 +0530

    day21

---
+++ day20/NameValidator/NameValidator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+
+        NameValidator();
+    }
+    static void NameValidator()
+    {
+
+        while (true)
+        {
+            Console.WriteLine("Enter your Name");
+            string userName = Console.ReadLine();
+            if (CheckUserName(userName))
+            {
+                Console.WriteLine($"Hey {userName} Welcome Back!");
+                break;
+            }
+            Console.WriteLine("Enter your name without special characters!");
+
+        }
+    }
+    static bool CheckUserName(string userName)
+    {
+
+        foreach (char c in userName)
+        {
+            if ((c == 32))
+                return false;
+            if ((c < 65 || c > 90) && (c < 97 || c > 122))
+                return false;
+        }
+        return true;
+    }
+}
---
+++ day21/CircleAbstactionApp/CircleAbstactionApp/Program.cs
@@ -0,0 +1,21 @@
+﻿
+namespace AbstractionCircleApp.Models
+{
+    class CircleShape
+    {
+        static void Main()
+        {
+            Circle circleSmall = new Circle();
+            circleSmall.radius = 10;
+            circleSmall.calculateArea();
+            Console.WriteLine("The radius is " + circleSmall.radius + " Area of circle1: " + circleSmall.calculateArea());
+
+            Circle circleBig = new Circle();
+            circleBig.radius = 100;
+            circleBig.calculateArea();
+            Console.WriteLine("The radius is " + circleBig.radius + " Area of circle1: " + circleBig.calculateArea());
+
+        }
+    }
+
+}
