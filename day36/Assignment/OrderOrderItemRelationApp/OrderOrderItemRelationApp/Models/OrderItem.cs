bc525c5cd45e2ac4a848fdcdeb32841ce1b46c2f
Akhilesh <akhilesh.rathnakaran@rrd.com>
2025-02-11 09:36:54 +0530

    day41

---
+++ day36/Assignment/OrderOrderItemRelationApp/OrderOrderItemRelationApp/Models/OrderItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderOrderItemRelationApp.Models
+{
+    public class OrderItem
+    {
+        public int ItemId { get; set; }
+        public string ItemName { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public double TotalPrice
+        {
+            get { return Price * Quantity; }
+        }
+    }
+}
