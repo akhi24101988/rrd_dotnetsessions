8d48ce01cd9e16f4e5ffea4b01b7ed62c9e3fa29
Akhilesh <akhilesh.rathnakaran@rrd.com>
2025-02-11 09:45:40 +0530

    day41

---
+++ day38/Assignment/SRPVoilationRefactoringApp/SRPVoilationRefactoringApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using SRPVoilationRefactoringApp.Model;
+
+namespace SRPVoilationRefactoringApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Invoice invoice = new Invoice(1, "Training Invoice", 100, .50);
+            PrintInvoice(invoice);
+        }
+
+        private static void PrintInvoice(Invoice invoice)
+        {
+            double tax = invoice.CalculateTax();
+            double discount = invoice.CalculateDiscount();
+            double finalCost = invoice.FinalCost();
+
+            string template = $"Id: {invoice.Id} " +
+                $", description: {invoice.Description}" +
+                $" cost is: {invoice.Cost}" +
+                $" tax amt is: {tax}" +
+                $" discount amt is: {discount}" +
+                $" Final cost is: {finalCost}";
+            Console.WriteLine(template);
+        }
+    }
+}
